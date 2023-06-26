3d2be69d1cb8f40139de5fe6669d0a6d3704a03e
Mark Karpeles <magicaltux@gmail.com>
2023-06-26 20:41:31 +0900

    add test with gif decoding + rendering

---
+++ gif_test.go
@@ -0,0 +1,75 @@
+package pixoo64_test
+
+import (
+	"image"
+	"image/draw"
+	"image/gif"
+	"log"
+	"net/http"
+	"testing"
+
+	"github.com/KarpelesLab/pixoo64"
+)
+
+func TestGif(t *testing.T) {
+	// this requires having an actual device
+	dev, err := pixoo64.FindName("Living")
+	if err != nil {
+		t.Fatalf("could not locate device: %s", err)
+		return
+	}
+	log.Printf("found device: %+v", dev)
+
+	resp, err := http.Get("https://pixeljoint.com/files/icons/avatar4_8.gif")
+	if err != nil {
+		t.Skipf("download failed: %s", err)
+		return
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		t.Skipf("download failed: %s", resp.Status)
+		return
+	}
+	src, err := gif.DecodeAll(resp.Body)
+
+	if len(src.Image) >= 60 {
+		t.Skipf("too many frames")
+		return
+	}
+
+	// apply gif images
+	prevImg := src.Image[0]
+	for n, img := range src.Image {
+		// build image
+		newImg := &image.Paletted{}
+		*newImg = *prevImg
+		newImg.Pix = dup(prevImg.Pix)
+
+		draw.Draw(newImg, newImg.Bounds(), img, image.Point{}, draw.Over)
+		src.Image[n] = newImg
+		prevImg = newImg
+	}
+
+	anim := pixoo64.NewAnim()
+	for n, i := range src.Image {
+		delay := src.Delay[n] * 10
+		log.Printf("delay = %d", delay)
+		imc, err := pixoo64.ConvertImage(i, delay)
+		if err != nil {
+			t.Errorf("convert image failed: %s", err)
+			continue
+		}
+		anim.AppendFrame(imc, 0)
+	}
+
+	// send it
+	anim.SendTo(dev)
+}
+
+func dup(v []byte) []byte {
+	r := make([]byte, len(v))
+	for n, b := range v {
+		r[n] = b
+	}
+	return r
+}
--- pixoo64_test.go
+++ pixoo64_test.go
@@ -14,7 +14,7 @@ import (
 
 func TestBasics(t *testing.T) {
 	// this requires having an actual device
-	dev, err := pixoo64.FindFirst()
+	dev, err := pixoo64.FindName("Living")
 	if err != nil {
 		t.Fatalf("could not locate device: %s", err)
 		return
@@ -29,9 +29,10 @@ func TestBasics(t *testing.T) {
 		// map[Brightness:100 ClockTime:60 CurClockId:64 GalleryShowTimeFlag:0 GalleryTime:60 GyrateAngle:0 LightSwitch:1 MirrorFlag:0 PowerOnChannelId:1 RotationFlag:0 SingleGalleyTime:-1 TemperatureMode:0 Time24Flag:1 error_code:0]
 	}
 
+	//dev.ShortBeeps(1)
 	//dev.ResetHttpGifId()
 	//dev.SelectChannel(0)
-	//return
+	return
 
 	anim := pixoo64.NewAnim()
 	dr := &font.Drawer{Src: image.NewUniform(color.RGBA{R: 0xff, G: 0xff, B: 0xff, A: 0xff}), Face: basicfont.Face7x13}
